b997098289069ac3d97923222b85dd15aaabb3b3
Ahmetcapar1 <100695576+Ahmetcapar1@users.noreply.github.com>
2025-01-13 00:21:03 +0300

    third

--- Tables.sql
+++ Tables.sql
@@ -1,76 +1,49 @@
-
-CREATE TABLE BOM (
-    ParentID int,
-    ChildID int,
-    Quantity int,
-    FOREIGN KEY (ParentID) REFERENCES ITEM(ItemID),
-    FOREIGN KEY (ChildID) REFERENCES ITEM(ItemID)
-)
-
 CREATE TABLE ITEM (
     ItemID INTEGER PRIMARY KEY AUTOINCREMENT,
-    LotSize int,
-    ItemName varchar(30)
-    
-)
+    LotSize INT,
+    ItemName VARCHAR(30)
+);
+
+CREATE TABLE BOM (
+    ParentID INT,
+    ChildID INT,
+    Quantity INT,
+    FOREIGN KEY (ParentID) REFERENCES ITEM(ItemID),
+    FOREIGN KEY (ChildID) REFERENCES ITEM(ItemID)
+);
 
 CREATE TABLE INVENTORY (
     InventoryID INTEGER PRIMARY KEY AUTOINCREMENT,
-    ItemID int,
-    Quantity int,
+    ItemID INT,
+    Quantity INT,
     FOREIGN KEY (ItemID) REFERENCES ITEM(ItemID)
-)
+);
 
-CREATE TABLE GROSS_REQUIREMENT(
-    RequirementID INTEGER PRIMARY KEY AUTOINCREMENT
-    ItemID int,
-    Quantity int,
-    Planned_Period int,
+CREATE TABLE GROSS_REQUIREMENT (
+    RequirementID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ItemID INT,
+    Quantity INT,
+    Planned_Period INT,
     FOREIGN KEY (ItemID) REFERENCES ITEM(ItemID),
     FOREIGN KEY (Planned_Period) REFERENCES PERIODS(PeriodID)
-)
-CREATE TABLE PERIODS(
-    PeriodID int,
-    PRIMARY KEY (PeriodID)
-)
-CREATE TABLE SYSTEM(
-    RequirementID int,
-    InventoryID int,
-    PeriodID int,
-    FOREIGN KEY (RequirementID) REFERENCES GROSS_REQUIREMENT(RequirementID)
-    FOREIGN KEY (InventoryID) REFERENCES INVENTORY(InventoryID)
+);
+
+CREATE TABLE PERIODS (
+    PeriodID INT PRIMARY KEY
+);
+
+CREATE TABLE SYSTEM (
+    RequirementID INT,
+    InventoryID INT,
+    PeriodID INT,
+    FOREIGN KEY (RequirementID) REFERENCES GROSS_REQUIREMENT(RequirementID),
+    FOREIGN KEY (InventoryID) REFERENCES INVENTORY(InventoryID),
     FOREIGN KEY (PeriodID) REFERENCES PERIODS(PeriodID)
-)
-INSERT INTO PERIODS(PeriodID)
+);
+
+INSERT INTO PERIODS (PeriodID)
 VALUES
-('1'),
-('2'),
-('3'),
-('4'),
-('5'),
-('6'),
-('7'),
-('8'),
-('9'),
-('10'),
-('11'),
-('12'),
-('13'),
-('14'),
-('15'),
-('16'),
-('17'),
-('18'),
-('19'),
-('20'),
-('21'),
-('22'),
-('23'),
-('24'),
-('25'),
-('26'),
-('27'),
-('28'),
-('29'),
-('30');
+(1), (2), (3), (4), (5), (6), (7), (8), (9), (10),
+(11), (12), (13), (14), (15), (16), (17), (18), (19), (20),
+(21), (22), (23), (24), (25), (26), (27), (28), (29), (30);
 
